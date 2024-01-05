01542c6373760f3f3ce0c26d3a8f148c8c891c2f
Ehwick <vargaserik1@gmail.com>
2024-01-05 02:03:32 -0600

    earthmap

--- personalwglobe/globe.js
+++ personalwglobe/globe.js
@@ -16,7 +16,7 @@ globerenderer.render(globescene, globecamera);
 // creating globe
 const geometry = new THREE.SphereGeometry( 0.85, 64, 64 ); 
 const material = new THREE.MeshPhongMaterial({ 
-  map: new THREE.TextureLoader().load('texture/blackmap.jpeg'),
+  map: new THREE.TextureLoader().load('texture/earthmap.jpeg'),
   bumpmap: new THREE.TextureLoader().load('texture/betterbumpmap.jpeg'),
   bumpScale: 1,
 }); 
