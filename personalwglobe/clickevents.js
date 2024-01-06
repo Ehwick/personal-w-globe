b3cbbe96f8aaa4fb8370dcde3893bef825370c06
Ehwick <vargaserik1@gmail.com>
2024-01-05 20:51:31 -0600

    clickevents update

--- personalwglobe/clickevents.js
+++ personalwglobe/clickevents.js
@@ -5,7 +5,14 @@ function toggleMenu() {
     icon.classList.toggle('open');
 };
 
+const hamcon = document.getElementById('#hamburger-icon');
+hamcon.addEventListener('click', toggleMenu);
+
+
 function sendEmail() 
 {
     window.location = "mailto:vargaserik1@gmail.com";
 };
+
+const email = document.getElementById('#email');
+email.addEventListener('click', sendEmail);
