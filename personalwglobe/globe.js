c894854505a65c07aeb1b3eeebf11ebd4cdf32d8
Ehwick <vargaserik1@gmail.com>
2024-01-05 22:06:17 -0600

    sphere position

--- personalwglobe/globe.js
+++ personalwglobe/globe.js
@@ -60,7 +60,7 @@ function onWindowResize() {
   }
   if (window.innerHeight < 500) {
     sphere.position.y = (0 + (window.innerHeight / 1000)) - 1.45;
-    sphere.position.x = -1.3;
+    sphere.position.x = -1.6;
   } 
   else {
     sphere.position.y = 0 - (window.innerHeight / 1000);
