7e2f06fe5d9621d24ca909fbfdcafc71ffee0707
Ehwick <vargaserik1@gmail.com>
2024-01-05 18:31:55 -0600

    js side event listeners

--- personalwglobe/personal.js
+++ personalwglobe/personal.js
@@ -68,7 +68,13 @@ function toggleMenu() {
     icon.classList.toggle('open');
 }
 
+const hamcon = document.getElementById("hamburger-icon");
+hamcon.addEventListener("click", toggleMenu);
+
 function sendEmail() 
 {
     window.location = "mailto:vargaserik1@gmail.com";
 }
+
+const email = document.getElementById("email");
+email.addEventListener("click", sendEmail);
